1066b104d6511dcadee536243d1c25622ea6b731
dms120 <dms.120@hotmail.com>
2024-11-08 13:48:02 +0000

    debug actions

--- app/actions.ts
+++ app/actions.ts
@@ -1,41 +1,49 @@
-'use server'
+"use server";
 
 import { EvmChain } from "@moralisweb3/common-evm-utils";
 import Moralis from "moralis";
 
 export async function initializeAPI() {
-    if (!Moralis.Core.isStarted) {
-        await Moralis.start({
-            apiKey: process.env.NEXT_MORALIS_API_KEY,
-        });
-    }
+  if (!Moralis.Core.isStarted) {
+    console.debug("Moralis will start");
+    await Moralis.start({
+      apiKey: process.env.NEXT_MORALIS_API_KEY,
+    });
+    console.debug("Moralis had started");
+  }
 }
 
 export async function resolveENSDomain(address: string) {
-    const response = await Moralis.EvmApi.resolve.resolveENSDomain({
-        domain: address,
-    });
+  console.debug("resolveENSDomain");
+  const response = await Moralis.EvmApi.resolve.resolveENSDomain({
+    domain: address,
+  });
 
-    const json = response?.toJSON();
-    return json?.address
+  const json = response?.toJSON();
+  return json?.address;
 }
 
 export async function getWalletNFTs(address: string) {
-    const chain = EvmChain.ETHEREUM;
-    const response = await Moralis.EvmApi.nft.getWalletNFTs({
-        address,
-        chain,
-    });
-    const list = response.toJSON().result;
-    return list
+  console.debug("getWalletNFTs");
+  const chain = EvmChain.ETHEREUM;
+  const response = await Moralis.EvmApi.nft.getWalletNFTs({
+    address,
+    chain,
+  });
+  const list = response.toJSON().result;
+  return list;
 }
 
 export async function getWalletTokenBalancesPrice(address: string) {
-    const chain = EvmChain.ETHEREUM;
-    const { response } = await Moralis.EvmApi.wallets.getWalletTokenBalancesPrice({
-        address,
-        chain,
-    });
+  console.debug("getWalletTokenBalancesPrice");
 
-    return response.toJSON().result
+  const chain = EvmChain.ETHEREUM;
+  const { response } = await Moralis.EvmApi.wallets.getWalletTokenBalancesPrice(
+    {
+      address,
+      chain,
+    }
+  );
+
+  return response.toJSON().result;
 }
